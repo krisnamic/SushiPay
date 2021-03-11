3c5e63d40436f333558ae93ea312a2f5784f7400
steven278 <darkavenger347@gmail.com>
2021-03-11 12:36:36 +0700

    Update Restaurant_UTS.sql

--- Restaurant_UTS.sql
+++ Restaurant_UTS.sql
@@ -1,46 +1,194 @@
-create database restaurant_uts;
-use restaurant_uts;
-CREATE TABLE account(
-	ID INT AUTO_INCREMENT PRIMARY KEY,
-	username VARCHAR(12),
-	email VARCHAR(50),
-	password VARCHAR(255),
-	firstName VARCHAR(50),
-	lastName VARCHAR(15),
-	birthDate DATE,
-	gender CHAR(1),
-	role CHAR(30)
-);
-CREATE TABLE pesanan(
-    ID_Pesanan INT AUTO_INCREMENT PRIMARY KEY,
-    ID_User INT(5),
-    tanggalPemesanan DATE,
-    FOREIGN KEY(id_user) REFERENCES account(ID)
-)engine=innoDB;
-CREATE TABLE detailPesanan(
-    ID_Menu INT AUTO_INCREMENT PRIMARY KEY,
-    hargaMenu INT(20),
-    jumlah INT(30), 
-    ID_Pesanan INT(5),
-    FOREIGN KEY (ID_Pesanan) REFERENCES pesanan(ID_Pesanan)
-)engine=innoDB;
-CREATE TABLE Kategori(
-    ID_Kategori INT AUTO_INCREMENT PRIMARY KEY,
-    namaKategori VARCHAR(100),
-    deskripsiKategori VARCHAR(255),
-    kategoriMenu VARCHAR(100),
-    gambarKategori VARCHAR(255)
-)engine=innoDB;
-CREATE TABLE menu(
-    namaMenu VARCHAR(50),
-    deskripsiMenu VARCHAR(200),
-    hargaMenu INT(20),
-    gambarMenu VARCHAR(100),
-    ID_Menu INT,
-    ID_Kategori INT,
-    FOREIGN KEY(ID_Menu) REFERENCES detailPesanan(ID_Menu),
-    FOREIGN KEY(ID_Kategori) REFERENCES kategori(ID_Kategori)
-)engine=innoDB;
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 11 Mar 2021 pada 06.30
+-- Versi server: 10.4.16-MariaDB
+-- Versi PHP: 7.4.12
 
--- insert into account()
--- values("","admin","adminsushipay@gmail.com","","");
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `restaurant_uts`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `account`
+--
+
+CREATE TABLE `account` (
+  `ID` int(11) NOT NULL,
+  `username` varchar(12) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `firstName` varchar(50) DEFAULT NULL,
+  `lastName` varchar(15) DEFAULT NULL,
+  `birthDate` date DEFAULT NULL,
+  `gender` char(1) DEFAULT NULL,
+  `role` char(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `account`
+--
+
+INSERT INTO `account` (`ID`, `username`, `email`, `password`, `firstName`, `lastName`, `birthDate`, `gender`, `role`) VALUES
+(1, 'admin', 'adminsushipay@gmail.com', '$2y$10$k.6LxhTDgky1YtqiINXTwOHZk/a9hBuqfEfgy7yRsI.490QgzsMf6', '-', '-', '0000-00-00', '-', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detailpesanan`
+--
+
+CREATE TABLE `detailpesanan` (
+  `ID_Menu` int(11) NOT NULL,
+  `hargaMenu` int(20) DEFAULT NULL,
+  `jumlah` int(30) DEFAULT NULL,
+  `ID_Pesanan` int(5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `ID_Kategori` int(11) NOT NULL,
+  `namaKategori` varchar(100) DEFAULT NULL,
+  `deskripsiKategori` varchar(255) DEFAULT NULL,
+  `kategoriMenu` varchar(100) DEFAULT NULL,
+  `gambarKategori` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `menu`
+--
+
+CREATE TABLE `menu` (
+  `namaMenu` varchar(50) DEFAULT NULL,
+  `deskripsiMenu` varchar(200) DEFAULT NULL,
+  `hargaMenu` int(20) DEFAULT NULL,
+  `gambarMenu` varchar(100) DEFAULT NULL,
+  `ID_Menu` int(11) DEFAULT NULL,
+  `ID_Kategori` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pesanan`
+--
+
+CREATE TABLE `pesanan` (
+  `ID_Pesanan` int(11) NOT NULL,
+  `ID_User` int(5) DEFAULT NULL,
+  `tanggalPemesanan` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indeks untuk tabel `detailpesanan`
+--
+ALTER TABLE `detailpesanan`
+  ADD PRIMARY KEY (`ID_Menu`),
+  ADD KEY `ID_Pesanan` (`ID_Pesanan`);
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`ID_Kategori`);
+
+--
+-- Indeks untuk tabel `menu`
+--
+ALTER TABLE `menu`
+  ADD KEY `ID_Menu` (`ID_Menu`),
+  ADD KEY `ID_Kategori` (`ID_Kategori`);
+
+--
+-- Indeks untuk tabel `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD PRIMARY KEY (`ID_Pesanan`),
+  ADD KEY `ID_User` (`ID_User`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `account`
+--
+ALTER TABLE `account`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `detailpesanan`
+--
+ALTER TABLE `detailpesanan`
+  MODIFY `ID_Menu` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `ID_Kategori` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `pesanan`
+--
+ALTER TABLE `pesanan`
+  MODIFY `ID_Pesanan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `detailpesanan`
+--
+ALTER TABLE `detailpesanan`
+  ADD CONSTRAINT `detailpesanan_ibfk_1` FOREIGN KEY (`ID_Pesanan`) REFERENCES `pesanan` (`ID_Pesanan`);
+
+--
+-- Ketidakleluasaan untuk tabel `menu`
+--
+ALTER TABLE `menu`
+  ADD CONSTRAINT `menu_ibfk_1` FOREIGN KEY (`ID_Menu`) REFERENCES `detailpesanan` (`ID_Menu`),
+  ADD CONSTRAINT `menu_ibfk_2` FOREIGN KEY (`ID_Kategori`) REFERENCES `kategori` (`ID_Kategori`);
+
+--
+-- Ketidakleluasaan untuk tabel `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD CONSTRAINT `pesanan_ibfk_1` FOREIGN KEY (`ID_User`) REFERENCES `account` (`ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
