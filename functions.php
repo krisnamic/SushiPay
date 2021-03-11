c944911468bd0cf78090cf6b9b763a4613f7b477
jryarianto <jerry.arianto@student.umn.ac.id>
2021-03-11 13:50:22 +0700

    comit on 'branch-php'

--- functions.php
+++ functions.php
@@ -1,40 +1,43 @@
 <?php
 
-function register($data)
-{
-    global $db;
+$db = mysqli_connect("localhost","root","","restaurant_uts");
 
-    $username = strtolower(stripslashes($data["username"]));
-    $password = mysqli_real_escape_string($db, $data["password"]);
-    $password2 = mysqli_real_escape_string($db, $data["password2"]);
+    function register($data){
+        global $db;
 
-
-    //checking if there is a same username into db
-    $result = mysqli_query($db, "SELECT username 
-                       FROM users 
-                       WHERE username = '$username'");
-
-    if (mysqli_fetch_assoc($result)) {
-        echo "<script>
-                alert('Username already taken! Please use another one!')
-              </script>";
-        return false;
-    }
+        $username = strtolower(stripslashes($data["username"]));
+        $password = mysqli_real_escape_string($db, $data["password"]);
+        $password2 = mysqli_real_escape_string($db, $data["password2"]);
 
 
-    //checking password return the same password confirmation
-    if ($password !== $password2) { ?>
-        <p id="err">Password doesnt match confirmation!</p>
-<?php return false;
-    }
+        //checking if there is a same username into db
+        $result = mysqli_query($db, "SELECT username 
+                        FROM users 
+                        WHERE username = '$username'");
+
+        if (mysqli_fetch_assoc($result)) {
+            echo "<script>
+                    alert('Username already taken! Please use another one!')
+                </script>";
+            return false;
+        }
 
 
-    //encrypt password
-    $password = password_hash($password, PASSWORD_DEFAULT);
+        //checking password return the same password confirmation
+        if ($password !== $password2) { ?>
+            <p id="err">Password doesnt match confirmation!</p>
+        <?php return false;
+        }
 
 
-    //adding user's data to db
-    mysqli_query($db, "INSERT INTO users VALUE('','$username','$password')");
+        //encrypt password
+        $password = password_hash($password, PASSWORD_DEFAULT);
 
-    return mysqli_affected_rows($db);
-} ?>
+
+        //adding user's data to db
+        mysqli_query($db, "INSERT INTO users VALUE('','$username','$password')");
+
+        return mysqli_affected_rows($db);
+    } 
+?>
+
--- index.php
+++ index.php
@@ -1,2 +1,30 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "restaurant_uts");
+
+session_start();
+
+    if(isset($_POST["logout"])){
+
+        echo "sadas";
+        $_SESSION = [];
+        session_unset();
+        session_destroy();
+        header("Location: login.php");
+        exit;
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <button class="button logout" type="submit" name="logout">Log out!</button>
+    </form>
+</body>
+</html>
