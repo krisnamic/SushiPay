49b09b94344c9012e47073f37e95ea20c92e6462
Michael KC <cahyadi.michael.mkc24@gmail.com>
2021-03-13 18:51:11 +0700

    Update Restaurant_UTS.sql

--- Restaurant_UTS.sql
+++ Restaurant_UTS.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 11 Mar 2021 pada 06.30
+-- Waktu pembuatan: 13 Mar 2021 pada 11.54
 -- Versi server: 10.4.16-MariaDB
 -- Versi PHP: 7.4.12
 
@@ -36,7 +36,7 @@ CREATE TABLE `account` (
   `lastName` varchar(15) DEFAULT NULL,
   `birthDate` date DEFAULT NULL,
   `gender` char(1) DEFAULT NULL,
-  `role` char(30) DEFAULT NULL
+  `role` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -44,7 +44,8 @@ CREATE TABLE `account` (
 --
 
 INSERT INTO `account` (`ID`, `username`, `email`, `password`, `firstName`, `lastName`, `birthDate`, `gender`, `role`) VALUES
-(1, 'admin', 'adminsushipay@gmail.com', '$2y$10$k.6LxhTDgky1YtqiINXTwOHZk/a9hBuqfEfgy7yRsI.490QgzsMf6', '-', '-', '0000-00-00', '-', 'admin');
+(1, 'admin', 'adminsushipay@gmail.com', '$2y$10$k.6LxhTDgky1YtqiINXTwOHZk/a9hBuqfEfgy7yRsI.490QgzsMf6', '-', '-', '0000-00-00', '-', 'admin'),
+(2, 'tes', 'tes@gmail.com', '$2y$10$ccFEgaGsgJjC6b6xHOKvpO6yvfE/lM7PRAbD1uTahQYqtU31IvUti', 'tes', 'tes', '2021-03-09', 'm', 'user');
 
 -- --------------------------------------------------------
 
@@ -53,10 +54,10 @@ INSERT INTO `account` (`ID`, `username`, `email`, `password`, `firstName`, `last
 --
 
 CREATE TABLE `detailpesanan` (
-  `ID_Menu` int(11) NOT NULL,
+  `ID_Pesanan` int(11) NOT NULL,
   `hargaMenu` int(20) DEFAULT NULL,
   `jumlah` int(30) DEFAULT NULL,
-  `ID_Pesanan` int(5) DEFAULT NULL
+  `ID_Menu` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -69,9 +70,22 @@ CREATE TABLE `kategori` (
   `ID_Kategori` int(11) NOT NULL,
   `namaKategori` varchar(100) DEFAULT NULL,
   `deskripsiKategori` varchar(255) DEFAULT NULL,
-  `kategoriMenu` varchar(100) DEFAULT NULL,
   `gambarKategori` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`ID_Kategori`, `namaKategori`, `deskripsiKategori`, `gambarKategori`) VALUES
+(1, 'Sushi', 'Sushi is a traditional Japanese dish of prepared vinegared rice, usually with some sugar and salt, accompanying a variety of ingredients, such as seafood, often raw, and vegetables.', 'Sushi.jpg'),
+(2, 'Surimi (Fish Ball)', 'Surimi refers to a paste made from fish or other meat. It can also refer to a number of East Asian foods that use that paste as their primary ingredient.', 'Surimi.jpg'),
+(3, 'Tempura', 'Tempura is a typical Japanese dish usually consisting of seafood, meat, and vegetables that have been battered and deep fried.', 'Tempura.jpg'),
+(4, 'Karaage', 'Karaage is a Japanese cooking technique in which various foods—most often chicken, but also other meat and fish—are deep fried in oil.', 'Karaage.jpg'),
+(5, 'Kushi Katsu', 'Kushikatsu, also known as kushiage, is a Japanese dish of deep-fried skewered meat and vegetables. In Japanese, kushi refers to the skewers used while katsu means a deep-fried cutlet of meat.', 'Kushi Katsu.jpg'),
+(6, 'Donburi', 'Donburi is a Japanese rice-bowl dish consisting of fish, meat, vegetables or other ingredients simmered together and served over rice.', 'Donburi.jpg'),
+(7, 'Dessert', 'Dessert  is a sweet course or dish (as of pastry or ice cream) usually served at the end of a meal.', 'Dessert.jpg'),
+(8, 'Drinks', 'We also serve many drinks for you to fresh your taste palate after eating heavy food with light refreshing drink like cold ocha.', 'Drinks.jpg');
 
 -- --------------------------------------------------------
 
@@ -80,13 +94,61 @@ CREATE TABLE `kategori` (
 --
 
 CREATE TABLE `menu` (
+  `ID_Menu` int(11) NOT NULL,
   `namaMenu` varchar(50) DEFAULT NULL,
   `deskripsiMenu` varchar(200) DEFAULT NULL,
   `hargaMenu` int(20) DEFAULT NULL,
   `gambarMenu` varchar(100) DEFAULT NULL,
-  `ID_Menu` int(11) DEFAULT NULL,
   `ID_Kategori` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `menu`
+--
+
+INSERT INTO `menu` (`ID_Menu`, `namaMenu`, `deskripsiMenu`, `hargaMenu`, `gambarMenu`, `ID_Kategori`) VALUES
+(1, 'California Roll', 'California Roll or California Maki is a makizushi sushi roll that is usually rolled inside-out, and containing cucumber, crab or imitation crab, and avocado.', 25000, 'California Roll.jpg', 1),
+(2, 'Crunchy Roll', 'Crunchy Roll is a roll with a center of a shrimp tempura, snow crab mix, cucumber, and avocado and then it is topped with crunchy tempura flakes.', 25000, 'Crunchy Roll.jpg', 1),
+(3, 'Spicy Tuna Roll', 'Spicy Tuna Roll is a makizushi roll that usually contains raw tuna, and spicy mayo or sriracha.', 30000, 'Spicy Tuna Roll.jpg', 1),
+(4, 'Salmon Mayo Roll', 'Salmon Mayo Roll is a makizushi with a blend of salmon, sweet mayo, and shoyu sauce.', 29000, 'Salmon Mayo Roll.jpg', 1),
+(5, 'Ebi Tempura Roll', 'Ebi Tempura Roll is a makizushi roll with a battered and deep fried shrimp that is topped with crunchy tempura flakes.', 23000, 'Ebi Tempura Roll.jpg', 1),
+(6, 'Salmon Deep Fried Roll', 'Salmon Deep Fried Roll is a makizushi roll with deep fried salmon, asparagus core, and panko crust.', 13000, 'Salmon Deep Fried Roll.jpg', 1),
+(7, 'Shrimp Bomb', 'Shrimp Bomb is an anti-personnel explosive device containing shrimps to increase its effectiveness at harming victims.', 24000, 'Shrimp Bomb.jpg', 2),
+(8, 'Fish Ball', 'Fish Ball is made of pastry dough wrapped in a fish filling.', 49000, 'Fish Ball.jpg', 2),
+(9, 'Cuttlefish Ball', 'Cuttlefish Ball is made of fish paste and cuttlefishes.', 49000, 'Cuttlefish Ball.jpg', 2),
+(10, 'Crab Ball', 'Crab Ball is a dish made with deep fried crab paste.', 19000, 'Crab Ball.jpg', 2),
+(11, 'Cheese Dumpling', 'Cheese Dumpling is a dish that consists of pieces of dough wrapped around a cheese filling.', 22000, 'Cheese Dumpling.jpg', 2),
+(12, 'Kakiage Crab Stick', 'Kakiage Crab Stick is a seafood tempura made of starch and finely pulverized white fish.', 28000, 'Kakiage Crab Stick.jpg', 3),
+(13, 'Kakiage Original', 'Kakiage is a type of tempura that consists of an assortment of seafood and thinly sliced vegetables.', 39000, 'Kakiage Original.jpg', 3),
+(14, 'Kakiage Shrimp', 'Kakiage Shrimp is made by batter-dipping and deep-frying a batch of ingredients such as shrimp bits (or a clump of small-sized shrimp).', 42000, 'Kakiage Shrimp.jpg', 3),
+(15, 'Shrimp Tempura', 'Shrimp Tempura or Ebi Ten is a deep fried shrimp coated with crispy tempura crumbs.', 18000, 'Shrimp Tempura.jpg', 3),
+(16, 'Dori Tempura', 'Dori Tempura is a deep fried John Dory fish coated wih crispy tempura crumbs.', 43000, 'Dori Tempura.jpg', 3),
+(17, 'Chicken Tempura', 'Chicken Tempura or Tori Ten is made of deep fried chicken with juicy & tender meat inside and crispy tempura crumbs outside.', 31000, 'Chicken Tempura.jpg', 3),
+(18, 'Karaage Original', 'Karaage is a Japanese cooking technique in which various foods—most often chicken, but also other meat and fish—are deep fried in oil.', 21000, 'Karaage Original.jpg', 4),
+(19, 'Karaage Spicy', 'Karaage Spicy is a Japanese cooking technique in which various foods—most often chicken, but also other meat and fish—are deep fried in oil with chili sauce', 43000, 'Karaage Spicy.jpg', 4),
+(20, 'Karaage Cheese', 'Karaage Cheese is a Japanese cooking technique in which various foods—most often chicken, but also other meat and fish—are deep fried in oil with cheese', 39000, 'Karaage Cheese.jpg', 4),
+(21, 'Karaage BBQ', ' Karaage BBQ is a Japanese cooking technique in which various foods—most often chicken, but also other meat and fish—are deep fried in oil and bbq sauce with BBQ flavor', 49000, 'Karaage BBQ.jpg', 4),
+(22, 'Ebi Furai', 'Shrimps or prawns are often prepared by frying, especially deep frying. This is popular in many countries, especially in Asia.', 23000, 'Ebi Furai.jpg', 5),
+(23, 'Dori Katsu', 'Dori Katsu is a katsu with dori fish.', 10000, 'Dori Katsu.jpg', 5),
+(24, 'Chicken Katsu', 'Chicken Katsu is a Japanese dish of fried chicken made with panko bread crumbs which is also popular in Australia, Hawaii, London, California, and other areas of the world.', 8000, 'Chicken Katsu.jpg', 5),
+(25, 'Calamari', 'The word calamari comes from the Italian for \"squid.\" In the United States, it generally refers to a battered and deep-fried appetizer served in restaurants and bars.', 11000, 'Calamari.jpg', 5),
+(26, 'Ebi Katsu', 'This popular Japanese dish is often found on fast-food menus and is served on a hamburger bun with lettuce and mayonnaise. You can also serve this with rice and Japanese curry sauce.', 14000, 'Ebi Katsu.jpg', 5),
+(27, 'Meaji Katsu', 'Meaji Katsu is a Japanese breaded and deep-fried ground meat patty; a fried meat cake.', 19000, 'Meaji Katsu.jpg', 5),
+(28, 'Donburi Satsuma-Age', 'Donburi Satsuma-Age is a Japanese \"rice-bowl dish\" consisting of fried fishcake and served over rice.', 18000, 'Donburi Satsuma-Age.jpg', 6),
+(29, 'Mix Karaage Set', 'Mix Karage Set is a Japanese \"rice-bowl dish\" consisting of mixed toppings and served over rice.', 22000, 'Mix Karage Set.jpg', 6),
+(30, 'Donburi Chicken Katsu', 'Donburi Chicken Katsu is a Japanese \"rice-bowl dish\" consisting of chicken katsu and served over rice.', 17000, 'Donburi Chicken Katsu.jpg', 6),
+(31, 'Donburi Seafood', 'Donburi Seafood is a Japanese \"rice-bowl dish\" consisting of seafood and served over rice.', 23000, 'Donburi Seafood.jpg', 6),
+(32, 'Donburi Chicken Teriyaki', 'Donburi Chicken Teriyaki is a Japanese \"rice-bowl dish\" consisting of chicken terriyaki and served over rice.', 14000, 'Donburi Chicken Teriyaki.jpg', 6),
+(33, 'Pudding Tiramisu', 'Tiramisu puddings are a class of dessert with tiramisu flavors.', 20000, 'Pudding Tiramisu.jpg', 7),
+(34, 'Pudding Choco Cheese', 'Choco cheese puddings are a class of dessert with choco cheese flavors.', 19000, 'Pudding Choco Cheese.jpg', 7),
+(35, 'Pudding Chocolate Deluxe', 'Chocolate puddings are a class of desserts with chocolate flavors.', 19000, 'Puding Chocolate Deluxe.jpg', 7),
+(36, 'Pudding Cream Cheese', 'Smooth and delicate triple layered Cream Cheese Pudding with cheese grates.', 20000, 'Pudding Cream Cheese.jpg', 7),
+(37, 'Cold Ocha', 'It is traditionally made using bancha tea leaves and unpolished brown rice grains. This gives the tea a distinctive roasted aroma that might remind you of popcorn, and a nutty taste with ice', 8000, 'Cold Ocha.jpg', 8),
+(38, 'Lychee Tea', 'Lychee Tea is a fruit tea that consists of lychee fruit, juice, flower petals, or extracts mixed with traditional black tea. ', 8000, 'Lychee Tea.jpg', 8),
+(39, 'Lemonade', 'Lemonade is a sweetened lemon-flavored beverage.', 8000, 'Lemonade.jpg', 8),
+(40, 'Lemon Tea', 'Lemon Tea is a refreshing tea where lemon juice is added in black or green tea. It soothes the throat, prevents cough and congestion, and helps in weight loss. ', 9000, 'Lemon Tea.jpg', 8),
+(41, 'dfaaaaa', 'we', 213111, '604b75f47c6fd.jpg', 4),
+(43, 'chicken soup', 'enak', 20000, '604c5775a8999.jpeg', 5);
 
 -- --------------------------------------------------------
 
@@ -97,8 +159,22 @@ CREATE TABLE `menu` (
 CREATE TABLE `pesanan` (
   `ID_Pesanan` int(11) NOT NULL,
   `ID_User` int(5) DEFAULT NULL,
-  `tanggalPemesanan` date DEFAULT NULL
+  `tanggalPemesanan` date DEFAULT NULL,
+  `waktuPemesanan` time DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pesanan`
+--
+
+INSERT INTO `pesanan` (`ID_Pesanan`, `ID_User`, `tanggalPemesanan`, `waktuPemesanan`) VALUES
+(1, 2, '2021-03-13', '00:00:00'),
+(2, 2, '2021-03-13', '22:22:22'),
+(4, 2, '2021-03-13', '10:10:12'),
+(6, 2, '2021-03-13', '11:45:06'),
+(7, 2, '2021-03-13', '11:45:30'),
+(8, 2, '2021-03-13', '11:52:36'),
+(9, 2, '2021-03-13', '17:53:06');
 
 --
 -- Indexes for dumped tables
@@ -114,8 +190,8 @@ ALTER TABLE `account`
 -- Indeks untuk tabel `detailpesanan`
 --
 ALTER TABLE `detailpesanan`
-  ADD PRIMARY KEY (`ID_Menu`),
-  ADD KEY `ID_Pesanan` (`ID_Pesanan`);
+  ADD PRIMARY KEY (`ID_Pesanan`),
+  ADD KEY `ID_Menu` (`ID_Menu`);
 
 --
 -- Indeks untuk tabel `kategori`
@@ -127,7 +203,7 @@ ALTER TABLE `kategori`
 -- Indeks untuk tabel `menu`
 --
 ALTER TABLE `menu`
-  ADD KEY `ID_Menu` (`ID_Menu`),
+  ADD PRIMARY KEY (`ID_Menu`),
   ADD KEY `ID_Kategori` (`ID_Kategori`);
 
 --
@@ -145,25 +221,25 @@ ALTER TABLE `pesanan`
 -- AUTO_INCREMENT untuk tabel `account`
 --
 ALTER TABLE `account`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT untuk tabel `detailpesanan`
---
-ALTER TABLE `detailpesanan`
-  MODIFY `ID_Menu` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `ID_Kategori` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_Kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT untuk tabel `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `ID_Menu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT untuk tabel `pesanan`
 --
 ALTER TABLE `pesanan`
-  MODIFY `ID_Pesanan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_Pesanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
@@ -173,14 +249,14 @@ ALTER TABLE `pesanan`
 -- Ketidakleluasaan untuk tabel `detailpesanan`
 --
 ALTER TABLE `detailpesanan`
-  ADD CONSTRAINT `detailpesanan_ibfk_1` FOREIGN KEY (`ID_Pesanan`) REFERENCES `pesanan` (`ID_Pesanan`);
+  ADD CONSTRAINT `detailpesanan_ibfk_1` FOREIGN KEY (`ID_Menu`) REFERENCES `menu` (`ID_Menu`),
+  ADD CONSTRAINT `detailpesanan_ibfk_2` FOREIGN KEY (`ID_Pesanan`) REFERENCES `pesanan` (`ID_Pesanan`);
 
 --
 -- Ketidakleluasaan untuk tabel `menu`
 --
 ALTER TABLE `menu`
-  ADD CONSTRAINT `menu_ibfk_1` FOREIGN KEY (`ID_Menu`) REFERENCES `detailpesanan` (`ID_Menu`),
-  ADD CONSTRAINT `menu_ibfk_2` FOREIGN KEY (`ID_Kategori`) REFERENCES `kategori` (`ID_Kategori`);
+  ADD CONSTRAINT `menu_ibfk_1` FOREIGN KEY (`ID_Kategori`) REFERENCES `kategori` (`ID_Kategori`);
 
 --
 -- Ketidakleluasaan untuk tabel `pesanan`
