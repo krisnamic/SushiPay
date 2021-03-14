b66a3f9d9d3f46c762069d0468e79d075430007b
jonathan <jonathan.franzeli@student.umn.ac.id>
2021-03-15 00:28:16 +0700

    tambah assets/img/logo, update logo di index login register checkout

--- checkout.php
+++ checkout.php
@@ -57,7 +57,7 @@
 
       <!-- <h1 class="logo mr-auto"><a href="index.html">Restaurantly</a></h1> -->
       <!-- Uncomment below if you prefer to use an image logo -->
-      <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>
+      <a href="index.html" class="logo mr-auto"><img src="assets/img/logo/logo-red.png" alt="" class="img-fluid"></a>
 
       <nav class="nav-menu d-none d-lg-block">
         <ul>
--- login.php
+++ login.php
@@ -101,7 +101,7 @@ if (isset($_POST["login"])) {
 
       <!-- <h1 class="logo mr-auto"><a href="index.html">Restaurantly</a></h1> -->
       <!-- Uncomment below if you prefer to use an image logo -->
-      <a href="index.php" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>
+      <a href="index.php" class="logo mr-auto"><img src="assets/img/logo/logo-red.png" alt="" class="img-fluid"></a>
 
       <nav class="nav-menu d-none d-lg-block">
         <ul>
